1d354a5bd5b2b417b417339dcdb4a20bdc45e4d1
sevilay.agil <sevilayagil@gmail.com>
2019-12-03 21:39:03 +0300

    custom methods with c#

---
+++ SeleniumFirst/Program.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumFirst
+{
+    class Program
+    {
+        IWebDriver driver = new ChromeDriver();
+        static void Main(string[] args)
+        {
+        }
+        [SetUp]
+        public void Initialize()
+        {
+            driver.Navigate().GoToUrl("http://www.executeautomation.com/demosite/index.html");
+        }
+        [Test]
+        public void ExecuteTest()
+        {
+            //Title 
+            SeleniumSetMethods.SelectDropDown(driver, "TitleId", "Mr.", "Id");
+            //Initial
+            SeleniumSetMethods.EnterText(driver, "Initial", "Sevo", "Name");
+            //Click
+            SeleniumSetMethods.Click(driver, "Save", "Name");
+        }
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+            Console.WriteLine("closed the browser");
+        }
+    }
+}
